56ea80e5ee02eeaca8abc8f4721b3f33eb5865a4
Alessio <rocchi.b.a@gmail.com>
2024-11-05 23:51:28 +0100

    Add async tests and update Settings config
    Introduce async tests for the application root endpoint to ensure it is functioning correctly. Additionally, update the Settings configuration to use the new `SettingsConfigDict` for environment variable handling.

--- app/config.py
+++ app/config.py
@@ -1,9 +1,9 @@
 import os
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
 
 class Settings(BaseSettings):
     rabbitmq_url: str = os.getenv("RABBITMQ_URL", "amqp://guest:guest@localhost:5672/")
     service_name: str = "coreService"
 
-    class Config:
-        env_file = ".env"
+    model_config = SettingsConfigDict(env_file=".env")
